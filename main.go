e608a958555f8b18162d21c9c1c3b9aa64b6ab27
Jadson Medeiros <developer.medeiros@gmail.com>
2022-02-08 21:44:32 -0300

    add function to show all students

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func showAllStudents(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"name": "Git Hub",
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/students", showAllStudents)
 	r.Run()
 }
