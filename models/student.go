0780614f943f3153cc80be2758c921b7e027cd2b
Jadson Medeiros <developer.medeiros@gmail.com>
2022-03-02 21:50:24 -0300

    added regex

--- models/student.go
+++ models/student.go
@@ -8,7 +8,7 @@ import (
 type Student struct {
 	gorm.Model
 	Name string `json:"name" validate:"nonzero"`
-	CPG  string `json:"cpg" validate:"len=11"`
+	CPG  string `json:"cpg" validate:"len=11, regexp=^[0-9]*$"`
 	//ID   string `json:"id"`
 }
 
